4f7cbde5e8b2f30bfeff675c8c5dcb22334879b2
Amina Javaid <103302290+aminajavaid30@users.noreply.github.com>
2024-02-14 00:17:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import google.generativeai as genai
 
 # Set Google API key
-os.environ['GOOGLE_API_KEY'] = 'AIzaSyCTxdhWdOtv6TcxbFZ2DcXV0TBTix_AnpY'
+os.environ['GOOGLE_API_KEY'] = 'YOUR_API_KEY'
 genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
 # Create the Model
